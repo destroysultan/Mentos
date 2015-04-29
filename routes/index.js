6cf529c360a1d93b885f30c92d3134fb22b8bbb3
Wen Li <wenli936@gmail.com>
2015-04-29 16:00:50 -0700

    separate slots and mentors

--- routes/index.js
+++ routes/index.js
@@ -7,22 +7,56 @@ var _ = require('underscore');
 router.get('/', function(req, res, next) {
 
 	// spreadsheet key is the long id in the sheets URL 
-	var my_sheet = new GoogleSpreadsheet('1j2dq5q4eKt8uQ5tIxpaBTjZFxNpyO1VTrMGBdP98m10');
+	var my_sheet = new GoogleSpreadsheet('1ik7jX0x0e1IuDzKT9YVKIVlLIVen7QtIHp11QakJmA8');
+	var password = "hpuykaprnehswzwr";
 
-		// # is worksheet id - IDs start at 1 
-	my_sheet.getRows(1, function(err, allSlots){
-	    console.log( 'pulled in '+allSlots.length + ' rows ');
+	var token = my_sheet.setAuth("wenli936@gmail.com", password, function() {
+	});	
 
+	my_sheet.getRows(2, function(err, allSlots){
+		var timeSlots = [];
 
+		//grab all the date/time slots from the original spreadsheet
+		for ( var i = 0; i < allSlots.length; i++){
+			var time = new Date(allSlots[i]['datetime']);
+			if (new Date() < time)
+				timeSlots.push(time.toString());
+		}
 
-	    //datetime, date doesn't work because theres no year, also reads as
-	    //a string
+		var options = {
+			orderby: "datetime"
+		}
 
-	    //mess with oauth somehow
+		//ordered list of mentors
+		my_sheet.getRows( 1, options, function(err, allMentors){
 
-	    res.render('index', { title: 'Mentos' , slots: allSlots});
-	   	});	
+			var pastMentors = [];
+			var upcoming = [];
 
+			for(var i = 0; i < allMentors.length; i++){
+				var mentorTime = new Date(allMentors[i]['datetime']);
+				var slotIndex = timeSlots.indexOf(mentorTime.toString());
+
+				//separate mentors by past and upcoming
+				if (mentorTime > new Date()) {
+					upcoming.push(allMentors[i]);
+				}
+				else {
+					pastMentors.push(allMentors[i]);
+				}
+
+				//remove taken slots from list of slots
+				if (slotIndex > -1){
+				 	timeSlots.splice(slotIndex, 1);
+				}
+			}
+			
+			//mentors is ordered list of mentors
+	    res.render('index', { title: 'Mentos' , upcoming: upcoming, slots: timeSlots, past: pastMentors.reverse()});
+
+		});
+	});
+	
 });
 
 module.exports = router;
