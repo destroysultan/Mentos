952a2f576fc103d0afd1d7acaa6e21a66ccee7e8
Troy Sultan <troysultan@gmail.com>
2015-05-01 16:46:02 -0700

    styling changes, rendering data from BE with handlebars, added navigation for tracks

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,62 @@
 var express = require('express');
 var router = express.Router();
-var GoogleSpreadsheet = require('google-spreadsheet');
+var GoogleSpreadsheet = require("google-spreadsheet");
 var _ = require('underscore');
 
-
-
-
 /* GET home page. */
-
 router.get('/', function(req, res, next) {
 
-	var my_sheet = new GoogleSpreadsheet('1HJetdqReHZq9rjK9HKhIZXdM6xs9mS7rgk4eod9cXfw');
+	// spreadsheet key is the long id in the sheets URL 
+	var my_sheet = new GoogleSpreadsheet('1ik7jX0x0e1IuDzKT9YVKIVlLIVen7QtIHp11QakJmA8');
+	var password = "hpuykaprnehswzwr";
 
-	// Without auth -- read only 
-	// IMPORTANT: See note below on how to make a sheet public-readable! 
-	// # is worksheet id - IDs start at 1 
-	my_sheet.getRows( 3, function(err, row_data){
-	    console.log(row_data);
-	    console.log(row_data[8]['datetime']);
-	})
+	var token = my_sheet.setAuth("wenli936@gmail.com", password, function() {
+	});	
 
-  res.render('index', { title: 'Welcome to Mentor Hub' });
+	my_sheet.getRows(6, function(err, allSlots){
+		var timeSlots = [];
+
+		//grab all the date/time slots from the original spreadsheet
+		for ( var i = 0; i < allSlots.length; i++){
+			var time = new Date(allSlots[i]['datetime']);
+			if (new Date() < time)
+				timeSlots.push(time.toString());
+		}
+
+		var options = {
+			orderby: "datetime"
+		}
+
+		//ordered list of mentors
+		my_sheet.getRows( 5, options, function(err, allMentors){
+
+			var pastMentors = [];
+			var upcoming = [];
+
+			for(var i = 0; i < allMentors.length; i++){
+				var mentorTime = new Date(allMentors[i]['datetime']);
+				var slotIndex = timeSlots.indexOf(mentorTime.toString());
+
+				//separate mentors by past and upcoming
+				if (mentorTime > new Date()) {
+					upcoming.push(allMentors[i]);
+				}
+				else {
+					pastMentors.push(allMentors[i]);
+				}
+
+				//remove taken slots from list of slots
+				if (slotIndex > -1){
+				 	timeSlots.splice(slotIndex, 1);
+				}
+			}
+			
+			//mentors is ordered list of mentors
+	    res.render('index', { title: 'Welcome to Mentor Hub' , upcoming: upcoming, slots: timeSlots, past: pastMentors.reverse()});
+
+		});
+	});
+	
 });
 
 module.exports = router;
